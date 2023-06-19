37b3187ca6d0efaf050eed10d11022a6a82faa86
MengxueTao <freya17606545318@163.com>
2023-06-19 21:34:02 +0800

    test

---
+++ C5_TD_CliffWalkingEnv.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm  # tqdm是显示循环进度条的库
+
+
+class CliffWalkingEnv:
+    def __init__(self, ncol, nrow):
+        self.ncol = ncol # 列
+        self.nrow = nrow  # 行
+        self.x = 0  # 记录当前智能体位置的横坐标
+        self.y = self.nrow - 1  # 记录当前智能体位置的纵坐标
+
+    def step(self, action):  # 外部调用这个函数来改变当前位置
+        # 区别在于：没有定义P矩阵
+        # 4种动作, change[0]:上, change[1]:下, change[2]:左, change[3]:右。坐标系原点(0,0)
+        # 定义在左上角
+        change = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        self.x = min(self.ncol - 1, max(0, self.x + change[action][0]))
+        self.y = min(self.nrow - 1, max(0, self.y + change[action][1]))
+        next_state = self.y * self.ncol + self.x
+        reward = -1
+        done = False
+                          # 第三行
+        if self.y == self.nrow - 1 and self.x > 0:  # 下一个位置在悬崖或者目标
+            done = True
+            if self.x != self.ncol - 1:# 不在第11列
+                reward = -100
+        return next_state, reward, done
+
+    def reset(self):  # 回归初始状态,坐标轴原点在左上角
+        self.x = 0 # 列0
+        self.y = self.nrow - 1 # 行 3
+        return self.y * self.ncol + self.x
